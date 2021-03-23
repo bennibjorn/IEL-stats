b65691300c5eacdb7295fca510944fb5b56a2339
Benni <bennibjorn@hotmail.com>
2021-03-23 02:04:48 +0000

    caching pro league standings

--- src/modules/league/league.module.ts
+++ src/modules/league/league.module.ts
@@ -1,10 +1,11 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { CacheModule, HttpModule, Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { Challengermode } from 'src/providers/challengermode/challengermode';
 import { LeagueController } from './league.controller';
 import { LeagueService } from './league.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register(), ScheduleModule.forRoot()],
   controllers: [LeagueController],
   providers: [LeagueService, Challengermode]
 })
--- src/modules/league/league.service.ts
+++ src/modules/league/league.service.ts
@@ -1,12 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { Cache } from 'cache-manager';
 import { Challengermode } from 'src/providers/challengermode/challengermode';
+import { StandingDTO } from 'src/providers/challengermode/challengermode.types';
 
 @Injectable()
 export class LeagueService {
 
-    constructor(private readonly cm: Challengermode) {}
+    constructor(private readonly cm: Challengermode, @Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+    private readonly cacheKey = 'proStandings';
+    private readonly logger = new Logger(LeagueService.name);
 
-    getProLeagueStandings() {
-        return this.cm.getProLeagueStandings();
+    async getProLeagueStandings() {
+        try {
+            await this.cacheManager.del(this.cacheKey);
+            const cached = await this.cacheManager.get<StandingDTO[]>(this.cacheKey);
+            return cached;
+        } catch (error) {
+            // nothing cached
+            this.logger.log(`${this.logger.getTimestamp()} - Nothing cached, getting initial data`);
+            return this.updateAndRetreiveStandings();
+        }
+    }
+
+    async updateAndRetreiveStandings() {
+        const updated = await this.cm.getProLeagueStandings();
+        await this.cacheManager.set(this.cacheKey, updated, { ttl: null });
+        return updated;
+    }
+    
+    @Cron('*/5 19,20,21,22 * * MON,TUE,THU,FRI')
+    manuallyUpdateStandingsCache() {
+        this.logger.log(`${this.logger.getTimestamp()} - Running scheduled standings update`);
+        this.updateAndRetreiveStandings();
     }
 }
