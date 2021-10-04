ebdf57469201bca8a8df581612e791cfe636e7ae
Benni <bennibjorn@hotmail.com>
2021-10-04 10:26:20 +0000

    Update swagger docs for schedule

--- src/modules/league/league.controller.ts
+++ src/modules/league/league.controller.ts
@@ -2,8 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { LeagueService } from './league.service';
-import { LeagueStandings } from './types';
-import { Tournament } from 'src/providers/challengermode/challengermode.types';
+import { LeagueRound, LeagueStandings } from './types';
 
 @ApiTags('league')
 @Controller('league')
@@ -23,14 +22,9 @@ export class LeagueController {
 	}
 
 	@Get('schedule')
-	@ApiOperation({ summary: 'Fetch standings from the pro league' })
+	@ApiOperation({ summary: 'Fetch the schedule from the pro league' })
 	@ApiResponse({ status: 200 })
-	@ApiQuery({ name: 'refreshCache', type: Boolean, required: false, description: 'Refreshes the cache if cacheSecret is also included'})
-	@ApiQuery({ name: 'cacheSecret', required: false, description: 'Secret key to enable manually refreshing the cache'})
-	getProLeagueSchedule(@Query('refreshCache') refreshCache?: string, @Query('cacheSecret') cacheSecret?: string): Promise<any> {
-		const shouldRefreshCache = 
-			refreshCache?.toLowerCase() === 'true' && 
-			cacheSecret === this.configService.get<string>('CACHE_SECRET_KEY');
+	getProLeagueSchedule(): Promise<LeagueRound[]> {
 		return this.leagueService.getProLeagueSchedule();
 	}
 }
