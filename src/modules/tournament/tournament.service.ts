a1e275b0bd9b3889d9eea87ef100ed4597ca9e89
Benni <bennibjorn@hotmail.com>
2023-08-07 22:14:51 +0000

    Fixed cache manager method

--- src/modules/tournament/tournament.service.ts
+++ src/modules/tournament/tournament.service.ts
@@ -1,16 +1,19 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { Challengermode } from 'src/providers/challengermode/challengermode';
+import { TournamentGraph } from 'src/providers/challengermode/challengermode.types';
 
 @Injectable()
 export class TournamentService {
 	constructor(private readonly cm: Challengermode, @Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 	async getTournamentById(tournamentId: string): Promise<any> {
 		const key = `tournament-${tournamentId}`;
-		const cached = await this.cacheManager.match(key);
+		const cached = await this.cacheManager.get<TournamentGraph>(key);
 		if (cached) {
 			return cached;
 		}
-		const tournament = await this.cm.tournamentGraph(tournamentId);
-		return tournament;
+		const tournamentResponse = await this.cm.tournamentGraph(tournamentId);
+		await this.cacheManager.set(key, tournamentResponse.tournament, { ttl: 30 });
+		return tournamentResponse.tournament;
 	}
 }
